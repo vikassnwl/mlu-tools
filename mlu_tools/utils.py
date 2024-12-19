0be3f8645b7cb4f3f41d5a8d8ea56a497d6a3eef
Vikas Sanwal <vikassnwl@gmail.com>
2024-12-19 21:57:57 +0530

    Add  param to  func

--- mlu_tools/utils.py
+++ mlu_tools/utils.py
@@ -5,6 +5,7 @@ import requests
 from tqdm import tqdm
 import os
 from .data_structure import TreeNode
+import gdown
 
 
 def set_global_seed(seed_value):
@@ -18,15 +19,20 @@ def set_global_seed(seed_value):
     tf.random.set_seed(seed_value)
 
 
-def download(file_url, file_save_path):
-    response = requests.get(file_url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
+def download(file_url, file_save_path, download_from="drive"):
+    if download_from == "web":
+        response = requests.get(file_url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
 
-    with open(file_save_path, "wb") as file:
-        with tqdm(total=total_size, unit="B", unit_scale=True) as bar:
-            for chunk in response.iter_content(chunk_size=1024):
-                file.write(chunk)
-                bar.update(len(chunk))
+        with open(file_save_path, "wb") as file:
+            with tqdm(total=total_size, unit="B", unit_scale=True) as bar:
+                for chunk in response.iter_content(chunk_size=1024):
+                    file.write(chunk)
+                    bar.update(len(chunk))
+    else:
+        FILE_ID = file_url.split("/")[-2]
+        download_url = f"https://drive.google.com/uc?id={FILE_ID}&export=download"
+        gdown.download(download_url, file_save_path)
 
 
 def tree(dir_pth):
