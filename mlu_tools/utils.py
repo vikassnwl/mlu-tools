31e90da9d2345fe6753be2b3b331ab35766fb820
Vikas Sanwal <vikassnwl@gmail.com>
2025-04-11 12:29:45 -0400

    Update 'unpack_archive' to extract and save archives having no root folder.

--- mlu_tools/utils.py
+++ mlu_tools/utils.py
@@ -117,31 +117,40 @@ def unpack_archive(file_path, target_dir=None, force=False):
     if file_path.endswith(".zip"):
         with zipfile.ZipFile(file_path, "r") as zip_ref:
             members = zip_ref.namelist()
-            root_dir = members[0].split("/")[0]
+
     elif file_path.endswith((".tar", ".tar.gz", ".tgz")):
         with tarfile.open(file_path, "r") as tar_ref:
             members = tar_ref.getnames()
-            root_dir = members[0].split("/")[0]
+
     else:
         print("Unsupported file type")
         return
 
-    # unpacked_dir = os.path.join(target_dir, root_dir)
-    unpacked_dir = target_dir
-    print(f"Unpacking {file_path} to {target_dir}...")
+    # CHECKING IF ARCHIVE CONTAINS A ROOT FOLDER OR NOT
+    archive_contains_a_root_folder = all([os.path.dirname(member) for member in members])
+    if archive_contains_a_root_folder:
+        root_dir = os.path.dirname(members[0])
+    else:
+        root_dir = os.path.splitext(os.path.basename(file_path))[0]
+
+    unpacked_dir = os.path.join(target_dir, root_dir)
+    print(f"Unpacking {file_path} to {unpacked_dir}...")
     if os.path.exists(unpacked_dir) and not force:
         print(f"{unpacked_dir} already exists. Skipping unpacking.\n")
         return
+
+    if not archive_contains_a_root_folder:
+        os.makedirs(unpacked_dir)
 
     # Unpack with progress bar
     if file_path.endswith(".zip"):
         with zipfile.ZipFile(file_path, "r") as zip_ref:
             for member in tqdm(members, desc="Extracting", unit="file"):
-                zip_ref.extract(member, target_dir)
+                zip_ref.extract(member, unpacked_dir)
     elif file_path.endswith((".tar", ".tar.gz", ".tgz")):
         with tarfile.open(file_path, "r") as tar_ref:
             for member in tqdm(members, desc="Extracting", unit="file"):
-                tar_ref.extract(member, target_dir)
+                tar_ref.extract(member, unpacked_dir)
 
     print(f"Archive unpacked to {unpacked_dir}\n")
     return unpacked_dir
